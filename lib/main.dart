180f77c45231fc7406b7804c9c5b93434f7065c5
“Siukhan” <siukhan@cuiatd.edu.pk>
2025-11-15 22:28:11 +0500

    variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,41 @@ void main() {
   print("Hello Dart World!");
   print("Sheikh Irfan Ullah Khan");
   print("I am interested to make mobile applcations");
-  
+
   // Make a Square Shape
   print("Square Shape:");
+  print("Task Completed");
   print("********");
   print("*      *");
   print("*      *");
   print("*      *");
   print("*      *");
   print("********");
+
+  // Make a pyramid Shape
+
+  print(" Pyramid Shape:");
+  print("    *\n   ***\n  *****\n *******\n*********");
+  print("Task Completed");
+  print("   *   ");
+  print("  ***  ");
+  print(" ***** ");
+  print("*******");
+
+  // In these lines of code we have understand the varaibles
+  // How to declare variables
+  // Data types in dart
+
+  int number; // Integer type variable
+
+  number = 50;
+
+  int numbertwo;
+
+  numbertwo = 100;
+
+  numbertwo = 40;
+
+  print(number);
+  print(numbertwo);
 }
