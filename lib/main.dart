83854dbd169dcf6ef69e1aacbf5b92c34aa49e35
“Siukhan” <siukhan@cuiatd.edu.pk>
2025-11-15 21:28:55 +0500

    first_dart_code

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Hello Dart World!");
+  print("Sheikh Irfan Ullah Khan");
+  print("I am interested to make mobile applcations");
+  
+  // Make a Square Shape
+  print("Square Shape:");
+  print("********");
+  print("*      *");
+  print("*      *");
+  print("*      *");
+  print("*      *");
+  print("********");
+}
